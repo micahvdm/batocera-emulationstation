a663d00734744b163cddf3df39114b5dce2c6ad3
DivideCo <micahvdm@gmail.com>
2024-08-08 00:33:47 +0200

    Update VolumeControl.cpp

--- es-core/src/VolumeControl.cpp
+++ es-core/src/VolumeControl.cpp
@@ -215,7 +215,7 @@ static PulseAudioControl PulseAudio;
 
 #if defined(__linux__)
 #if defined(_RPI_) || defined(_VERO4K_)
-		std::string VolumeControl::mixerName = "PCM";
+		std::string VolumeControl::mixerName = "Headphone";
 #else
 		std::string VolumeControl::mixerName = "Master";
 #endif
